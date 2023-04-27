2ff2725fd7493d69a5ff1c7b8dd05957bfd6378a
Lisa Nguyen <lisanguyensc@gmail.com>
2023-04-27 17:07:39 -0400

    Added component files and folders

---
+++ src/components/party-cards/party-cards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './party-cards.css'
+
+const partyCards = () => {
+  return (
+    <div>partyCards</div>
+  )
+}
+
+export default partyCards
---
+++ src/components/search/search-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './search.css'
+
+const searchComponent = () => {
+  return (
+    <div>searchComponent</div>
+  )
+}
+
+export default searchComponent
