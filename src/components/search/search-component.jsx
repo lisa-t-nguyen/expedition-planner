bef93ee007c5b6cd56ed4bf076759153bd81237d
Lisa Nguyen <lisanguyensc@gmail.com>
2023-05-03 16:02:27 -0400

    Added search input, submit button, and background image

--- src/components/search/search-component.jsx
+++ src/components/search/search-component.jsx
@@ -3,9 +3,10 @@ import './search.css'
 
 const SearchComponent = () => {
   return (
-    <div>
-      <h1>Search Component</h1>
-      </div>
+    <div className='search-component'>
+      <input type='text' placeholder='Search for player...' className='search-input'></input>
+      <button type='submit' className='submit-button'>Submit</button>
+    </div>
   )
 }
 
