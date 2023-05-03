c695f8c5270575a2a461b04e995a9fc674da4c39
Lisa Nguyen <lisanguyensc@gmail.com>
2023-05-03 16:46:56 -0400

    Set expedition planner container height to 100vh, styling updates

--- src/components/expedition-planner/expedition-planner.jsx
+++ src/components/expedition-planner/expedition-planner.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './expedition-planner.css'
 import Search from '../search/search'
 import Header from '../header/header'
 import Footer from '../footer/footer'
 
 const ExpeditionPlanner = () => {
   return (
-    <div>
+    <div className="expedition-planner-container">
         <Header/>
         <Search/>
         <Footer/>
