5a535544ebd11f613d6c51a44d7d0e311637866f
Lisa Nguyen <lisanguyensc@gmail.com>
2023-05-04 15:32:00 -0400

    Positioned footer properly and added styling to submit button

--- src/components/expedition-planner/expedition-planner.jsx
+++ src/components/expedition-planner/expedition-planner.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './expedition-planner.css'
 import Search from '../search/search'
 import Header from '../header/header'
+import PartyCards from '../party-cards/party-cards'
 import Footer from '../footer/footer'
 
 const ExpeditionPlanner = () => {
@@ -9,6 +10,7 @@ const ExpeditionPlanner = () => {
     <div className="expedition-planner-container">
         <Header/>
         <Search/>
+        <PartyCards/>
         <Footer/>
     </div>
   )
