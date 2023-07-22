fd4f83eda8ee9b7b4f39fd39f0d3496fc48f8fbc
Andy <andypham12854@gmail.com>
2023-07-22 15:54:50 -0400

    Added Remove button to hover options (functionality TBD)

--- expedition-planner/src/components/hover-options/hover-options.jsx
+++ expedition-planner/src/components/hover-options/hover-options.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './hover-options.css'
 
 const HoverOptions = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <>
-      <div className="hover-options-container">
-
+      <div className="hover-options-container"
+           onMouseEnter={() => setIsHovering(true)}
+           onMouseLeave={() => setIsHovering(false)}>
+        { isHovering && 
+          <div className="hover-options">
+            <button>Remove</button>
+          </div> 
+        }
       </div>
     </>
   )
